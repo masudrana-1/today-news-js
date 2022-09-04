0d457a49287828912c58e97dfffa95b4bb25b3fc
masudrana-1 <masudrana58855@gmail.com>
2022-09-04 07:17:56 +0600

    add style in main page

--- js/default.js
+++ js/default.js
@@ -4,9 +4,9 @@ const defaultDisplay = () => {
 
     const newsContainer = document.getElementById('news-container');
     const newDiv = document.createElement('div');
-    newDiv.classList.add('bg-light', 'p-4', 'rounded-4')
+    newDiv.classList.add('bg-light', 'p-5', 'rounded-4')
     newDiv.innerHTML = `
-    <h1 class="text-center my-5">"Please select a category"</h1>
+    <h1 class="text-center default-text">"Please select a category"</h1>
     `;
 
     newsContainer.appendChild(newDiv);
